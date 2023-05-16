4109ae8ba9f546bf8c524b715bac5e7867d1de6d
AngusMcPherson <110000587+AngusMcPherson@users.noreply.github.com>
2023-05-16 23:29:58 +0930

    Add files via upload

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -3,53 +3,63 @@
 #include <vector>
 #include <string>
 #include "Autocomplete.h"
- using namespace std;
+using namespace std;
 
-TrieNode::TrieNode() {
-    children =  vector<TrieNode*>(MAX_NODE, nullptr);
+TrieNode::TrieNode()
+{
+    children = vector<TrieNode *>(MAX_NODE, nullptr);
     isEndOfWord = false;
 }
 
-void Autocomplete::insertHelper(TrieNode* node, string word) {
-    for (char c : word) {
-        int index = c - 'a';
-        if (node->children[index] == nullptr) {
-            node->children[index] = new TrieNode();
-        }
-    node = node->children[index];
-}
-node->isEndOfWord = true;
+void Autocomplete::insertHelper(TrieNode *node, string word)
+{
+    int temp = -1;
+    for (int i = 0; i < word.length(); i++)
+    {
+        temp = word[i] - 'a';
+        if (node->children[temp] == nullptr)
+            node->children[temp] = new TrieNode();
+        node = node->children[temp];
+    }
+    node->isEndOfWord = true;
 }
 
-void Autocomplete::getSuggestionsHelper(TrieNode* node, string partialWord, vector<string>& suggestions) {
-if (node->isEndOfWord) {
-    suggestions.push_back(partialWord);
-}
-for (int i = 0; i < MAX_NODE; i++) {
-    if (node->children[i] != nullptr) {
-        getSuggestionsHelper(node->children[i], partialWord + char('a' + i), suggestions);
+void Autocomplete::getSuggestionsHelper(TrieNode *node, string partialWord, vector<string> &suggestions)
+{
+    if (node->isEndOfWord)
+    {
+        suggestions.push_back(partialWord);
+    }
+    for (int i = 0; i < MAX_NODE; i++)
+    {
+        if (node->children[i] != nullptr)
+            getSuggestionsHelper(node->children[i], partialWord + char('a' + i), suggestions);
     }
 }
-}
 
-
-Autocomplete::Autocomplete() {
+Autocomplete::Autocomplete()
+{
     root = new TrieNode();
 }
 
-void Autocomplete::insert(string word) {
+void Autocomplete::insert(string word)
+{
     insertHelper(root, word);
 }
 
-vector<string> Autocomplete::getSuggestions(string partialWord) {
+vector<string> Autocomplete::getSuggestions(string partialWord)
+{
     vector<string> suggestions;
-    TrieNode* node = root;
-    for (char c : partialWord) {
-    int index = c - 'a';
-        if (node->children[index] == nullptr) {
+    TrieNode *node = root;
+    int p = -1;
+    for (int i = 0; i < partialWord.length(); i++)
+    {
+        p = partialWord[i] - 'a';
+        if (node->children[p] == nullptr)
+        {
             return suggestions;
         }
-        node = node->children[index];
+        node = node->children[p];
     }
     getSuggestionsHelper(node, partialWord, suggestions);
     return suggestions;
--- Autocomplete.h
+++ Autocomplete.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 const int MAX_NODE = 26;
+
 class TrieNode {
 public:
     std::vector<TrieNode*> children;
@@ -19,11 +20,7 @@ private:
     void getSuggestionsHelper(TrieNode* node, std::string partialWord, std::vector<std::string>& suggestions);
 public:
     Autocomplete();
-    void insert(std::string word);
+    void insert(std::string word);  
     std::vector<std::string> getSuggestions(std::string partialWord);
 };
 #endif
-      
-    
-    
-
